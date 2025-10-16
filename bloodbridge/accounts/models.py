86bb3b49cee974083f19332f2b27945dfeda4f6f
Lue <escasinasluezyldominic@gmail.com>
2025-10-16 21:40:14 +0800

    created custom user model and migrated to db

--- bloodbridge/accounts/models.py
+++ bloodbridge/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ('regular', 'Regular'),
+        ('hospital', 'Hospital'),
+        ('admin', 'Admin'),
+    ]
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='donor')
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
