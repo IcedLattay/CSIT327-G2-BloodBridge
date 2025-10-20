1cc49a36ecd3dbfd732207cd7ce7cd8ce8bdc6bf
Lue <escasinasluezyldominic@gmail.com>
2025-10-20 19:00:48 +0800

    feature(donations): created the request model

--- bloodbridge/donations/models.py
+++ bloodbridge/donations/models.py
@@ -21,3 +21,17 @@ class Donation(models.Model):
 
     def __str__(self):
         return f"{self.donor.username} donated to {self.hospital} on ({self.date})"
+    
+
+class Request(models.Model):
+    requester = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='requests')
+    date = models.DateField()
+    blood_type = models.ForeignKey(BloodType, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20)
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        compatiblebTypes = ", ".join([bType.type for bType in self.blood_type.compatible_with.all()])
+        return f"{self.requester.username} has requested either of the blood types {compatiblebTypes} for blood type {self.blood_type.type} on ({self.date})"
