fff7b06bd81cb4aed5a42df18cce97203ec8deef
Lue <escasinasluezyldominic@gmail.com>
2025-10-20 16:45:57 +0800

    feature(donations): created bloodtype model

--- bloodbridge/donations/models.py
+++ bloodbridge/donations/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+from accounts.models import CustomUser
+
+class BloodType(models.Model):
+    type = models.CharField(max_length=3, unique=True)  # e.g., "A+", "O-", etc.
+    compatible_with = models.ManyToManyField("self", symmetrical=False, blank=True)
+
+    def __str__(self):
+        compatiblebTypes = ", ".join([bType.type for bType in self.compatible_with.all()])
+        return f"{self.type} (compatible with: {compatiblebTypes if compatiblebTypes else 'None'})"
