7a25555e0bc43d781b16f64d8fe61d048dd48d09
Lue <escasinasluezyldominic@gmail.com>
2025-10-25 16:44:01 +0800

    feature(donations): added a login_required decorated for request and donation history views for authentication restrictions

--- bloodbridge/donations/views.py
+++ bloodbridge/donations/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 # Donation view
+@login_required(login_url='/')
 def donation_history_view(request):
     return render(request, 'donation_history.html')
 
 # Request view
+@login_required(login_url='/')
 def request_history_view(request):
     return render(request, 'request_history.html')
