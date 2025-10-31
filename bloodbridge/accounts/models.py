b3cc7e5b70796cbd9bfcb2590116c4d2c0b4d586
Lue <escasinasluezyldominic@gmail.com>
2025-11-01 00:03:17 +0800

    feature: create hospital, appointment, and request models and made migrations

--- bloodbridge/accounts/models.py
+++ bloodbridge/accounts/models.py
@@ -4,16 +4,10 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    ROLE_CHOICES = [
-        ('user', 'User'),
-        ('hospital', 'Hospital'),
-        ('admin', 'Admin'),
-    ]
-
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='donor')
+    is_available = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.username} ({self.role})"
+        return f"{self.username}"
 
 
 class Profile(models.Model):
@@ -32,3 +26,43 @@ class Profile(models.Model):
             f"Email: {self.user.username}\n"
             f"Blood Type: {self.blood_type.type if self.blood_type else 'N/A'}"
         )
+    
+
+class Hospital(models.Model):
+    name = models.CharField(max_length=255)
+    is_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Request(models.Model):
+    URGENCY_CHOICES = [
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    ]
+
+    hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
+    blood_type = models.ForeignKey('donations.BloodType', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    urgency = models.CharField(max_length=10)
+    notes = models.TextField(blank=True, null=True)
+    time_open = models.TimeField()
+    time_close = models.TimeField()
+    days_open = models.CharField(max_length=255)  # Could store as comma-separated, e.g. "Mon,Tue,Wed"
+
+    def __str__(self):
+        return f"{self.hospital.name} - {self.blood_type} ({self.urgency})"
+
+
+class Appointment(models.Model):
+    donor = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    request = models.ForeignKey(Request, on_delete=models.CASCADE)
+    date = models.DateField()
+    time_start = models.TimeField()
+    time_end = models.TimeField()
+
+    def __str__(self):
+        return f"{self.donor.username} - {self.request.blood_type.type_name} on {self.date}"
