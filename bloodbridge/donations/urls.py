ff60feed982cfdbde23d659970ef18e1058f8a85
Lue <escasinasluezyldominic@gmail.com>
2025-11-02 00:24:34 +0800

    feature: created post views for manage user requests feature

--- bloodbridge/donations/urls.py
+++ bloodbridge/donations/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('donation-history/', views.donation_history_view, name='donation_history'),
     path('request-history/', views.request_history_view, name='request_history'),
+    path('appointments/<int:request_id>/cancel/', views.cancel_appointment, name='cancel_appointment'),
+    path('appointments/<int:request_id>/approve/', views.approve_appointment, name='approve_appointment'),
 ]
