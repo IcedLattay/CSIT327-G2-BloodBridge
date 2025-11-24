e6463b77bbe321be2e01b9b845a9369fd053965f
Lue <escasinasluezyldominic@gmail.com>
2025-11-24 19:09:14 +0800

    feature: made a signal that creates instances of blood stock when a hospital user is created"

--- bloodbridge/accounts/signals.py
+++ bloodbridge/accounts/signals.py
@@ -1,9 +1,21 @@
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import CustomUser, Profile
+from .models import CustomUser, Profile, HospitalBloodStock
+from donations.models import BloodType
 
 @receiver(post_save, sender=CustomUser)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=CustomUser)
+def create_hospital_stock(sender, instance, created, **kwargs):
+    if created and instance.role == 'hospital':
+        blood_types = BloodType.objects.all()
+        for bt in blood_types:
+            HospitalBloodStock.objects.create(
+                hospital=instance,
+                blood_type=bt,
+                units_available=0
+            )
